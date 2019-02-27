e3d967f9473af8314f48842ae1c34d34530e599b
Nabil Benketaf <nbenketaf@gmail.com>
2019-02-27 08:57:04 +0100

    Fix require in setup.clj

--- src/io/aviso/logging/setup.clj
+++ src/io/aviso/logging/setup.clj
@@ -2,7 +2,7 @@
   "Enables pretty exception reporting in the REPL and/or console, and sets up pretty exception logging
 when using clojure.tools.logging. Also installs an default uncaught exception handler, and initializes
 logging correlation id."
-  (:require io.aviso.logging.correlation
+  (:require [io.aviso.logging.correlation]
             [io.aviso.repl :as repl]
             [io.aviso.logging :as logging]))
 
